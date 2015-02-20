78415cddbd07ee2aa09f3a4c235c73349a6e36ff
Shimran <shimranbgeorge@gmail.com>
2015-02-20 11:11:28 -0800

    Modify spec error coverage

--- spec/specs.js
+++ spec/specs.js
@@ -2,7 +2,7 @@ describe("Pizza", function() {
   describe("numSlices()", function() {
     it("Outputs the number of pizzas given the size and type of pizza")
 
-    it("defaults to zero if invalid input is given", function(){
+    it("returns 'ERROR...' if invalid input is given", function(){
       var testPizza = Object.create(Pizza);
       expect(testPizza.numSlices()).to.equal("ERROR: Please choose an appropriate topping and size");
     });
@@ -24,25 +24,25 @@ describe("Pizza", function() {
       var testPizza = Object.create(Pizza);
       expect(testPizza.numSlices("pepperoni",10)).to.equal(6);
     })
-    it("returns '8' slices for a small, 10 inch pizza", function(){
+    it("returns '8' slices for a small, 10 inch pepperoni pizza", function(){
       var testPizza = Object.create(Pizza);
       expect(testPizza.numSlices("pepperoni",11)).to.equal(8);
     })
-    it("returns '8' slices for a small, 10 inch pizza", function(){
+    it("returns '8' slices for a small, 10 inch pepperoni pizza", function(){
       var testPizza = Object.create(Pizza);
       expect(testPizza.numSlices("pepperoni",12)).to.equal(10);
     })
-    it("returns 'ERROR' for cheese options other than 10,11,12 ", function(){
+    it("returns 'ERROR...' for cheese options other than 10,11,12 ", function(){
       var testPizza = Object.create(Pizza);
       expect(testPizza.numSlices("cheese",15)).to.equal("ERROR: Please choose an appropriate topping and size");
     })
 
-    it("returns 'ERROR' for pepperonni options other than 10,11,12 ", function(){
+    it("returns 'ERROR...' for pepperonni options other than 10,11,12 ", function(){
       var testPizza = Object.create(Pizza);
       expect(testPizza.numSlices("pepperoni",15)).to.equal("ERROR: Please choose an appropriate topping and size");
     })
 
-    it("returns 'ERROR' for options other than cheese  || pepperoni ", function(){
+    it("returns 'ERROR...' for options other than cheese  || pepperoni ", function(){
       var testPizza = Object.create(Pizza);
       expect(testPizza.numSlices("pineapple",10)).to.equal("ERROR: Please choose an appropriate topping and size");
     })
